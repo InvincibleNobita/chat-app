addb8e1c1c95a3c65977fa58b16a98d6e971c313
Manish P <invincible.manish1417@gmail.com>
2022-02-07 21:37:21 +0530

    modified-main-added all the endpoints, modifed-config/db-added mongodb

--- config/db.py
+++ config/db.py
@@ -1,3 +1,15 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import pymongo
+from bson.objectid import ObjectId
+from model.pymongo_model import SimpleModel, DiffHistoryModelV1, DiffHistoryModelV2
 
-conn=MongoClient(process.env.MONGO_URI)
+MONGO_KEY = os.getenv('MONGO_KEY')
+client = pymongo.MongoClient(MONGO_KEY)
+db = client["myFirstDatabase"]
+class User(SimpleModel):
+    collection = db.user 
+#print(os.environ['MONGO_URI'])
+#conn=MongoClient(os.environ['MONGO_URI'])
--- main.py
+++ main.py
@@ -1,33 +1,45 @@
-from concurrent.futures import process
+
 from typing import Optional
 
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+import pymongo
+from bson.objectid import ObjectId
+from model.pymongo_model import SimpleModel, DiffHistoryModelV1, DiffHistoryModelV2
+from models.user import User
+from models.message import Message
+from schemas.user import serializeDict,serializeList
 
-from routes.user import user
-from routes.message import message
+load_dotenv()
 
 
+MONGO_KEY = os.getenv('MONGO_URI')
+client = pymongo.MongoClient(MONGO_KEY)
+db = client["PyAssignment"]
 
 app = FastAPI()
-app.include_router(user)
-app.include_router(message)
 
-#print(MONGO_URI)
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_messages():
+    return serializeList(db.message.find())
 
 @app.post("/signup")
-def signup(username: str, password: str):
+def signup(newUser: User):
+    print(newUser)
+    db.user.insert_one(dict(newUser))
+    return serializeList(db.user.find())
 
-    return {"User created succeessfullyy"}
+@app.post("/send-chat")
+def send_chat(newMessage: Message):
+    db.message.insert_one(dict(newMessage))
+    return serializeList(db.message.find())
+
 
 @app.post("/login")
-def login(username: str, password: str):
+def login(existingUser: User):
+    user = serializeList(db.user.find_one({'username':existingUser.username}))
+    print(user)
+    return {user}
 
-    return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
